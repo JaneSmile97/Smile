3dedd892c4c31725a4fa58220584e0a9ea54bf1d
JaneSmile97 <jilujane@gmail.com>
2016-08-23 11:02:35 +0530

    Create S.c

---
+++ S.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b;
+ clrscr();
+ printf("\n Enter the Elements");
+ scanf("%d %d",&a,&b);
+ if(a>b)
+ {
+ printf("A is greater then B");
+ }
+ else
+ {
+ printf("B is greater than A");
+ }
+ return 0;
+ }
