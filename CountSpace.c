ecdf8ecee7c9cc886098e6e703037ee2b8325656
Jane Smile <jilujane@gmail.com>
2018-03-13 16:10:56 +0530

    Create CountSpace.c

---
+++ CountSpace.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+ int main()
+ {
+   int i,spaces;
+   char a[100];
+   printf("enter the string\n");
+   scanf("%s",a);
+    for(i = 0; a[i] != '\0'; i++)
+ {
+     if (a[i]=' ')
+     {
+          spaces++;
+     }
+  }
+   printf("the no. of spaces in the string is %d\n",spaces);
+   
+ }
